6c8d685b0e7441e8e098244b6483fe7370a790e8
sometimes-maybegood <maratakhun@gmail.com>
2025-03-10 16:40:31 +0300

    1

--- Урок 10Flask-sqlalchemy/data/loginform.py
+++ Урок 10Flask-sqlalchemy/data/loginform.py
@@ -7,6 +7,6 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     email = EmailField('Почта', validators=[DataRequired()])
-    pasыword = PasswordField('Пароль', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Зарегестрироваться')
